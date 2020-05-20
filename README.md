25c34678626c5edca3f2c60be4706bbf6f38f717
Gautam Gadipudi <gautam.96.gadipudi@gmail.com>
2020-05-20 00:40:48 -0400

    Revise word count
    * Use packages instead of modules
    * Add finally block when reading file
    * Add exception handling in main method
    * Use WordCounter class instead of individual methods

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python-word-count-illustration
 A specific exercise on Python concepts related Exception handling, Class, package, module, file handling.
+
+Usage:
+```bash
+python3 word_count.py
+```
+
+OR 
+
+```bash
+python3 word_count.py <file_path>
+```
---
+++ lib/util.py
@@ -0,0 +1,28 @@
+'''
+    Author: Gautam Gadipudi
+'''
+from sys import argv
+
+def get_file_name() -> str:
+    '''
+        Get name of file from command line argument or as user input
+    '''
+    file_name = ''
+    if len(argv) == 2:
+        file_name = argv[1]
+    else:
+        file_name = input("Please enter the file path: ")
+    return file_name 
+
+def read_file(file: str) -> list:
+    '''
+        Read file and return it's content.
+    '''
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            result = f.read()
+            return result
+    except IOError as e:
+        print(f"Unable to read file {file}")
+    finally:
+        f.close()
